185cabe37395f8ba12d0fc9a0b662173324e6cb3
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2023-06-15 13:09:05 +0530

    v

--- first/firstapp/views.py
+++ first/firstapp/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def f1(request):
     return HttpResponse("<a href=http://127.0.0.1:8000/admin/>Admin</a>")
 def f2(request):
-    return HttpResponse("<h1>Click above link to go to admin page</h1>")
+    return render(request,'medicare-plus/index.html')
