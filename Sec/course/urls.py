77019a934c532a85f543ef3270b654b1b5f97258
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2024-01-30 16:26:04 +0530

    bmv

--- Sec/course/urls.py
+++ Sec/course/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 urlpatterns = [
        path('', views.subj),
        path('regfor/',views.regfor,),
+       path('save/',views.saver)
 ]
--- Sec/course/views.py
+++ Sec/course/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from course.forms import RegistrationForm
+from .models import Student
 
 def subj(request):
     return render(request, 'course/topic.html')
@@ -7,3 +8,11 @@ def subj(request):
 def regfor(request):
     a=RegistrationForm()
     return render(request, 'course/regfor.html',{'form':a})
+
+def saver(request):
+    fm=RegistrationForm(request.POST)
+    r=fm.roll
+    nm=fm.name
+    
+    reg=Student(name=nm,roll=r)
+    reg.save
